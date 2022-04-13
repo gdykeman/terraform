1dfd79b8e287b364c21a0d88cef1000f2459da32
gdykeman <gerald.dykeman@gmail.com>
2022-04-13 11:40:45 -0400

    update

--- azure/main.tf
+++ azure/main.tf
@@ -30,7 +30,7 @@ resource "azurerm_resource_group" "rg" {
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "network" {
   name                = var.network
-  resource_group_name = azurerm_resource_group.gdykeman-rg.name
-  location            = azurerm_resource_group.gdykeman-rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
 }
